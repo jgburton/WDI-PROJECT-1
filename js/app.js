4cd3fdb47a4a850d7ba4e5f984683befced985b7
jgburton <jgranthamburton@gmail.com>
2016-12-10 15:28:13 +0000

    Piano set up with audio linked

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-console.log('loaded');
+// Piano pianoGame- Pseudo
+
+// - Make a grid for keys 7 keys (C(C#)D(D#)EF(F#)G(G#)A(A#)B) - possibly #/b\'s also?
+// - Get audio set up, sample of each individual pitched note
+// - Assign to each corresponding piano key in grid
+// - Map to MacBook keyboard: ASDFGHJ === Notes: CDEFGAB
+// - Make a sequence of a certain length (option: random OR series of preset
+// sequence.
+// - Light up the keys when played
+// - Record user input
+// - Check sequence against user input
+// - Display result
+// - Start again with a harder sequence
+// If user enters fail, restart game
+
+window.addEventListener('DOMContentLoaded', start);
+
+
+function start(){
+  var white = document.getElementsByTagName('li');
+  addListener(white);
+  function addListener(white){
+    for (var i = 0; i < white.length; i++){
+      white[i].addEventListener('click', playSound);
+    }
+  }
+  function playSound(){
+    var soundId = 's' + this.id;
+    var correctSound = document.getElementById(soundId);
+    correctSound.play();
+  }
+}
+//
+// }
+// (new Audio('audio/A.wav')).play()
